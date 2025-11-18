94f221a637e4604bb097b38e1d9032c2027ec2dd
sharvil31 <amburlesarvil@gmail.com>
2025-11-18 09:12:13 +0530

    Remove duplicates(sorted array) 18-11-2025

---
+++ Remove_duplicates(sorted array)/script.js
@@ -0,0 +1,16 @@
+const arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+const removeDuplicates = (arr) => {
+  let x = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[x]) {
+      x = x + 1;
+      arr[x] = arr[i];
+    }
+  }
+
+  return (x + 1)
+};
+
+console.log(removeDuplicates(arr));
