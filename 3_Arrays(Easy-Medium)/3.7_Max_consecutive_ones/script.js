8264659f8d7d76f9aaa979732a571b1a5ae2a3cd
sharvil31 <amburlesarvil@gmail.com>
2025-11-23 15:28:30 +0530

    3.7_Max_consecutive_ones 23-11-2025

---
+++ 3_Arrays(Easy-Medium)/3.7_Max_consecutive_ones/script.js
@@ -0,0 +1,26 @@
+const arr = [1, 1, 0, 1, 1, 0, 1, 1, 1];
+
+const maxConsOnes = (arr) => {
+  let onesCount = 0;
+  let maxCount = 0;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] !== 1) {
+  //       maxCount = Math.max(maxCount, onesCount);
+  //       onesCount = 0;
+  //     } else {
+  //       onesCount++;
+  //     }
+  //   }
+
+  //   return Math.max(maxCount, onesCount);
+
+  // shorter version
+  for (const num of arr) {
+    onesCount = num === 1 ? onesCount + 1 : 0;
+    maxCount = Math.max(maxCount, onesCount);
+  }
+
+  return maxCount;
+};
+
+console.log(maxConsOnes(arr));
