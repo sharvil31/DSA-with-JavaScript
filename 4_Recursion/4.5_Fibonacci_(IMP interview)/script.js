efc51e851e282d04704d31e068a844725facda77
sharvil31 <amburlesarvil@gmail.com>
2025-11-29 09:44:19 +0530

    4.5_Fibonacci_(IMP interview) 29-11-2025

--- 4_Recursion/4.5_Fibonacci_(IMP interview)/script.js
+++ 4_Recursion/4.5_Fibonacci_(IMP interview)/script.js
@@ -3,35 +3,46 @@
 // [0, 1, 2, 3, 5, 8]
 
 // Iterative Approach
-const findNthNum = (n) => {
-  //// With Array
-  // let arr = [0, 1];
-  //   for (i = 2; i <= n; i++) {
-  //     let sum = arr[i - 1] + arr[i - 2];
-  //     arr.push(sum);
-  //   }
-  //   return arr[n];
+// const findNthNum = (n) => {
+//   //// With Array
+//   // let arr = [0, 1];
+//   //   for (i = 2; i <= n; i++) {
+//   //     let sum = arr[i - 1] + arr[i - 2];
+//   //     arr.push(sum);
+//   //   }
+//   //   return arr[n];
 
-  // time complexity = O(n)
-// space complexity = O(n) // storing every Fibonacci number in an array of size n.
+//   // time complexity = O(n)
+// // space complexity = O(n) // storing every Fibonacci number in an array of size n.
 
+//   // Without Array
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
 
-  // Without Array
-  if (n === 0) return 0;
-  if (n === 1) return 1;
+//   let prev1 = 0;
+//   let prev2 = 1;
 
-  let prev1 = 0;
-  let prev2 = 1;
+//   for (i = 2; i <= n; i++) {
+//     let curr = prev1 + prev2;
+//     prev1 = prev2;
+//     prev2 = curr;
+//   }
+//   return prev2;
 
-  for (i = 2; i <= n; i++) {
-    let curr = prev1 + prev2;
-    prev1 = prev2;
-    prev2 = curr;
-  }
-  return prev2;
+//   // time complexity = O(n)
+// // space complexity = O(1) // not using any extra array space
+// };
 
-  // time complexity = O(n)
-// space complexity = O(1) // not using any extra array space
+// console.log(findNthNum(6));
+
+// Recursive Approach
+const fib = (n) => {
+  if (n <= 1) return n;
+
+  return fib(n - 1) + fib(n - 2);
 };
 
-console.log(findNthNum(6));
+console.log(fib(5));
+
+// time complexity = O(2^n) // Very bad -> we use Dynamic Programming to improve such time complexity
+// space complexity = O(n)
