52d149ff517136b7cebc9e5677065cc408c1b9d1
sharvil31 <amburlesarvil@gmail.com>
2025-12-14 18:52:04 +0530

    5 Generate substrings 14-12-2025

---
+++ 4_Recursion/Medium_Level/5_Generate_substrings/script.js
@@ -0,0 +1,20 @@
+const generateSubStrings = (str) => {
+  const res = [];
+
+  const helper = (start) => {
+    if (start === str.length) return;
+
+    let temp = "";
+    for (let i = start; i < str.length; i++) {
+      temp += str[i];
+      res.push(temp);
+    }
+
+    helper(start + 1);
+  };
+
+  helper(0);
+  return res;
+};
+
+console.log(generateSubStrings("abc"));
