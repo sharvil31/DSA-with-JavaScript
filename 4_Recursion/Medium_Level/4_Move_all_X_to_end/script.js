941561306ae41e9bb3a88bc5834165823792f8ff
sharvil31 <amburlesarvil@gmail.com>
2025-12-15 08:31:43 +0530

    4 Move all X to end 15-12-2025

--- 4_Recursion/Medium_Level/4_Move_all_X_to_end/script.js
+++ 4_Recursion/Medium_Level/4_Move_all_X_to_end/script.js
@@ -1,11 +1,11 @@
-const moveToEnd = (str) => {
+const moveXToEnd = (str) => {
   if (str.length === 0) return "";
 
-  const rest = moveToEnd(str.slice(1));
+  const rest = moveXToEnd(str.slice(1));
 
   if (str[0] === "x") return rest + "x";
 
   return str[0] + rest;
 };
 
-console.log(moveToEnd("axxbdxce"));
+console.log(moveXToEnd("axxbdxce"));
